33cc2eed87bd605e1ab2149eb019a1a73f12dcde
amshel <amshelhack3r@gmail.com>
2019-07-16 17:48:12 +0000

    remove functions add classes

--- main.js
+++ main.js
@@ -1,33 +1,35 @@
-//
-function genAttrbSvg(circle, attributes) {
-    const x1 = circle.x;
-    const y1 = circle.y - circle.r;
-    const y2 = y1 - (3 * attributes);
-    const p$=y2+3;
-      
-  
+class MutallEllipse {
+    constructor(pointX, pointY, radius, attributes) {
+        this.x = pointX;
+        this.y = pointY;
+        this.r = radius;
+        this.attributes = attributes;
+    }
+
+    genAttribSvg() {
+        const x1 = this.x;
+        const y1 = this.y - this.r;
+        const y2 = y1 - (3 * this.attributes.length);
+        
         return {
-        x1 ,
-        y1,
-        x2 : x1,
-        y2 ,
-         }
+            x1,
+            y1,
+            x2: x1,
+            y2,
+        }
     }
 
-    function genPoint(y1,y2,numattr){
-        const point=(y2-y1)/5;
-        
-       let data = {}
-        
-       let c = 0;
-       for (let index = 1;  index <= numattr; index++) {
-           data[`p${index}`] = y2+c ;
-           c = c - point;
-       }
+    genPoint() {
+        const line = this.genAttribSvg();
+        const space = (line.y2 - line.y1) / this.attributes.length;
+        let data = {}
+    
+        let c = 0;
+        for (let index = 1; index <= this.attributes.length; index++) {
+            data[`p${index}`] = line.y2 + c;
+            c = c - space;
+        }
         return data
-
     }
- 
-
-
+}
 
